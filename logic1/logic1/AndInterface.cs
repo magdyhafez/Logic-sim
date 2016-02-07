deba63c2563d8d4c1e7a551aa8c74b65e4b5d380
Mohamed Ismail <mihafez.mi@gmail.com>
2016-02-07 16:25:07 +0200

    moving anywhere

---
+++ logic1/logic1/AndInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logic1
+{
+    public partial class AndInterface : UserControl
+    {
+        private Point MouseDownLocation;
+        public AndInterface()
+        {
+            InitializeComponent();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && checkBox2.Checked)
+                radioButton1.Checked = true;
+            else
+                radioButton1.Checked = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && checkBox2.Checked)
+                radioButton1.Checked = true;
+            else
+                radioButton1.Checked = false;
+        }
+
+     
+
+        private void AndInterface_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                MouseDownLocation = e.Location;
+        }
+
+        private void AndInterface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - MouseDownLocation.X;
+                this.Top = e.Y + this.Top - MouseDownLocation.Y;
+
+            }
+        }
+
+        private void AndInterface_MouseEnter(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void AndInterface_MouseLeave(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.None; 
+        }
+
+ 
+        
+    }
+}
---
+++ logic1/logic1/XnorInterface.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logic1
+{
+    public partial class XnorInterface : UserControl
+    {
+        private Point MouseDownLocation;
+        public XnorInterface()
+        {
+            InitializeComponent();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!((!(checkBox1.Checked) && (checkBox2.Checked)) || (!(checkBox2.Checked) && (checkBox1.Checked))))
+                radioButton1.Checked = true;
+            else
+                radioButton1.Checked = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!((!(checkBox1.Checked) && (checkBox2.Checked)) || (!(checkBox2.Checked) && (checkBox1.Checked))))
+                radioButton1.Checked = true;
+            else
+                radioButton1.Checked = false;
+        }
+
+
+
+        private void XnorInterface_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                MouseDownLocation = e.Location;
+        }
+
+        private void XnorInterface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - MouseDownLocation.X;
+                this.Top = e.Y + this.Top - MouseDownLocation.Y;
+
+            }
+        }
+
+        private void XnorInterface_MouseEnter(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void XnorInterface_MouseLeave(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.None;
+        }
+
+        private void XnorInterface_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+
+        private void XnorInterface_Load_1(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+    }
+}
