d122a04d9452056058f72813fdc268826baf2b69
Mohamed Ismail <mihafez.mi@gmail.com>
2016-02-07 13:44:57 +0200

    all gates can be drawn and moved anywhere on the form

---
+++ gates can be drawn and moved anywhere on the form/logic1/logic1/AndInterface.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace logic1
+{
+    partial class AndInterface
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.node1 = new logic1.Node();
+            this.node3 = new logic1.Node();
+            this.node2 = new logic1.Node();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(176, 65);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 107);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::logic1.Properties.Resources.And_Logic_Functions_Digital_Electronics_clip_art_medium;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel1.Location = new System.Drawing.Point(22, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(138, 147);
+            this.panel1.TabIndex = 3;
+            // 
+            // node1
+            // 
+            this.node1.BackColor = System.Drawing.Color.Silver;
+            this.node1.Location = new System.Drawing.Point(176, 89);
+            this.node1.Name = "node1";
+            this.node1.Size = new System.Drawing.Size(10, 11);
+            this.node1.TabIndex = 6;
+            // 
+            // node3
+            // 
+            this.node3.BackColor = System.Drawing.Color.Silver;
+            this.node3.Location = new System.Drawing.Point(0, 136);
+            this.node3.Name = "node3";
+            this.node3.Size = new System.Drawing.Size(10, 11);
+            this.node3.TabIndex = 5;
+            // 
+            // node2
+            // 
+            this.node2.BackColor = System.Drawing.Color.Silver;
+            this.node2.Location = new System.Drawing.Point(0, 11);
+            this.node2.Name = "node2";
+            this.node2.Size = new System.Drawing.Size(10, 11);
+            this.node2.TabIndex = 4;
+            // 
+            // AndInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.node1);
+            this.Controls.Add(this.node3);
+            this.Controls.Add(this.node2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton1);
+            this.DoubleBuffered = true;
+            this.Name = "AndInterface";
+            this.Size = new System.Drawing.Size(190, 150);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AndInterface_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.AndInterface_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.AndInterface_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AndInterface_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private Node node2;
+        private Node node3;
+        private Node node1;
+    }
+}
---
+++ gates can be drawn and moved anywhere on the form/logic1/logic1/switch_input.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace logic1
+{
+    partial class switch_input
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.shapeContainer2 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape1.BorderColor = System.Drawing.Color.YellowGreen;
+            this.ovalShape1.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape1.Location = new System.Drawing.Point(2, 3);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(35, 27);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.shapeContainer2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(42, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // shapeContainer2
+            // 
+            this.shapeContainer2.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer2.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer2.Name = "shapeContainer2";
+            this.shapeContainer2.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape1});
+            this.shapeContainer2.Size = new System.Drawing.Size(42, 35);
+            this.shapeContainer2.TabIndex = 0;
+            this.shapeContainer2.TabStop = false;
+            // 
+            // switch_input
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "switch_input";
+            this.Size = new System.Drawing.Size(50, 42);
+            this.MouseEnter += new System.EventHandler(this.switch_input_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.switch_input_MouseLeave);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer2;
+    }
+}
